a58d91abdc3f0aabf80240fbfc4b2ec7ab9ee942
Ha Nguyen <mjboy1412@gmail.com>
2021-08-23 11:20:27 +0700

    change config.ts

--- config.ts
+++ config.ts
@@ -4,6 +4,6 @@ export const FIREBASE_PROJECT_ID = process.env.FIREBASE_PROJECT_ID;
 export const FIREBASE_STORAGE_BUCKET = process.env.FIREBASE_STORAGE_BUCKET;
 export const FIREBASE_MESSAGE_SENDER_ID = process.env.FIREBASE_MESSAGE_SENDER_ID;
 export const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID;
-export const FIREBASE_PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY;
+export const FIREBASE_PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n");
 export const FIREBASE_CLIENT_EMAIL = process.env.FIREBASE_CLIENT_EMAIL;
 export const FIREBASE_DATABASE_URL = process.env.FIREBASE_DATABASE_URL;
